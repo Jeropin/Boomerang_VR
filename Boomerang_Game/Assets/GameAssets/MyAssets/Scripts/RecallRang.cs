56c5b976f1afdc86b00d1d4853265384538debd3
Jerome Pineda <70554316+Jeropin@users.noreply.github.com>
2023-04-20 07:41:27 -0700

    Initial Commit

---
+++ Boomerang_Game/Assets/GameAssets/MyAssets/Scripts/RecallRang.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RecallRang : MonoBehaviour
+{
+    public InputActionProperty recallRang;
+    public Transform transformHand;
+    public Transform transformRang;
+    public GameObject rang;
+    public float speed = 25;
+    // private Transform transformHand = Handheld
+    // private Transform transformRang = rang.GetComponent<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(recallRang.action.ReadValue<float>());
+        if(recallRang.action.ReadValue<float>() == 1){
+            Recall();
+        }
+        else{
+             rang.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    void Recall()
+    {
+        Debug.Log("I have been recalled");
+        rang.transform.position = Vector3.MoveTowards(rang.transform.position, transformHand.position, 20 * Time.deltaTime);
+        rang.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
