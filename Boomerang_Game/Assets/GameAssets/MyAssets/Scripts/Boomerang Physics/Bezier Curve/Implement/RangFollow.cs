f24625d2b025b34e1270f6a9b863edfd0a74334e
Jerome Pineda <70554316+Jeropin@users.noreply.github.com>
2023-04-29 11:57:12 -0700

    feat: Trying to add Dynamic Throwing Speed

--- Boomerang_Game/Assets/GameAssets/MyAssets/Scripts/Boomerang Physics/Bezier Curve/Implement/RangFollow.cs
+++ Boomerang_Game/Assets/GameAssets/MyAssets/Scripts/Boomerang Physics/Bezier Curve/Implement/RangFollow.cs
@@ -7,6 +7,7 @@ public class RangFollow : MonoBehaviour
 {
     public RangPhysics curve;
     public ControlPointsSet setReturn;
+    public ControllerVelocity controllerVelocity;
     public float speed;
     private float sampleTime;
     void Start(){
@@ -24,12 +25,56 @@ public class RangFollow : MonoBehaviour
         transform.position = curve.evaulate(sampleTime);
         transform.forward = curve.evaulate(sampleTime + 0.001f) - transform.position;
         
-        // Debug.Log(curve.evaulate(sampleTime + 0.001f));
-
-        // Debug.Log(sampleTime);
         if(sampleTime >= 1f){
             toggleScript();
         }
     }
 
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// // Boomerang Object following the curve created
+// public class RangFollow : MonoBehaviour
+// {
+//     public RangPhysics curve;
+//     public ControlPointsSet setReturn;
+//     public ControllerVelocity controllerVelocity;
+//     public float speed;
+//     private float sampleTime;
+//     private bool isMagnitude = true;
+//     void Start(){
+//         sampleTime =0f;
+//     }
+
+//     public void toggleScript(){
+//         sampleTime = 0f;
+//         // speed = controllerVelocity.Magnitude;
+//         isMagnitude = !isMagnitude;
+//         gameObject.GetComponent<RangFollow>().enabled = !gameObject.GetComponent<RangFollow>().enabled;
+//     }
+
+//     void Update()
+//     {
+//         // Debug.Log(speed);
+//         if(isMagnitude){
+//             speed = controllerVelocity.Magnitude;
+//             isMagnitude = !isMagnitude;
+//         }
+
+//         sampleTime += Time.deltaTime*speed;
+//         transform.position = curve.evaulate(sampleTime);
+//         transform.forward = curve.evaulate(sampleTime + 0.001f) - transform.position;
+        
+//         // Debug.Log(curve.evaulate(sampleTime + 0.001f));
+
+//         // Debug.Log(sampleTime);
+//         if(sampleTime >= 1f){
+//             toggleScript();
+//         }
+//     }
+
+// }
+
---
+++ Boomerang_Game/Assets/GameAssets/MyAssets/Scripts/Hand/ControllerVelocity.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// using UnityEngine.XR;
+// using UnityEngine.XR.Interaction.Toolkit;
+
+public class ControllerVelocity : MonoBehaviour
+{
+    public InputActionProperty velocityProperty;
+    public Vector3 Velocity {get; private set; } = Vector3.zero;
+    public float Magnitude;
+
+    private void Update(){
+        Velocity = velocityProperty.action.ReadValue<Vector3>();
+        // Debug.Log(Velocity.x);
+        Magnitude = Mathf.Sqrt(Mathf.Pow(Velocity.x, 2) + Mathf.Pow(Velocity.y, 2) + Mathf.Pow(Velocity.z, 2));
+        Debug.Log("Magnitude from hand: " + Magnitude);
+        // Debug.Log(Mathf.Sqrt(Mathf.Pow(Velocity.x, 2) + Mathf.Pow(Velocity.y, 2) + Mathf.Pow(Velocity.z, 2)));
+    }
+}
